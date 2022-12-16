dd3ed18689989eddce7e02a2a13118725a7a57d4
corner <corner3499@gmail.com>
2022-12-16 16:40:55 +0900

    --

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,8 @@
 <template>
-    <div class="py-3">
-        <div>
-            <div
-                class="max-w-sm rounded overflow-hidden shadow-lg mb-4 dark:bg-gray-900"
-            >
+    <div class="py-3 snap-x">
+
+        <div class="snap-center">
+            <div class="max-w-sm rounded overflow-hidden shadow-lg mb-4 dark:bg-gray-900">
                 <img
                     class="w-full"
                     src="https://tailwindcss.com/img/card-top.jpg"
@@ -28,6 +27,27 @@
             <NuxtLink to="/about" >
                 <ButtonDarkMode />
             </NuxtLink>
+        </div>
+
+        <div class="snap-x flex ...">
+            <div class="snap-start ...">
+                <img src="https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
+            </div>
+            <div class="snap-start ...">
+                <img src="https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
+            </div>
+            <div class="snap-start ...">
+                <img src="https://images.unsplash.com/photo-1622890806166-111d7f6c7c97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
+            </div>
+            <div class="snap-start ...">
+                <img src="https://images.unsplash.com/photo-1590523277543-a94d2e4eb00b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
+            </div>
+            <div class="snap-start ...">
+                <img src="https://images.unsplash.com/photo-1575424909138-46b05e5919ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
+            </div>
+            <div class="snap-start ...">
+                <img src="https://images.unsplash.com/photo-1559333086-b0a56225a93c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
+            </div>
         </div>
 
     </div>

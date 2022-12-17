f493dcf3c63d3aa17c4f3bbeecc59650d35b5727
corner <corner3499@gmail.com>
2022-12-17 23:50:45 +0900

    composables useAuth

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,9 @@
 <template>
-    <div>
-        <div class="dark:bg-gray-800 dark:text-gray-200">
-            <p class="p-4 pb-2">
-                <select
-                    v-model="$colorMode.preference"
-                    class="border w-24 h-8 dark:bg-gray-900 dark:text-white dark:border-gray-700"
-                >
-                    <option value="system">System</option>
-                    <option value="light">Light</option>
-                    <option value="dark">Dark</option>
-                </select>
-            </p>
-        </div>
+    <div class="dark:bg-black">
 
-        <Navbar />
+        <Navbar/>
 
-        <slot class="mx-auto p-4" />
+        <slot class="mx-auto p-4"/>
 
     </div>
 </template>

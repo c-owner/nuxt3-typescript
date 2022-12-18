1441f5d025b93a7f4c77252bca71ff018d9fe344
corner <corner3499@gmail.com>
2022-12-18 20:50:50 +0900

    register form

--- pages/register.vue
+++ pages/register.vue
@@ -11,6 +11,7 @@ const errors = ref(new Map())
 let response = ref<FormValidation>({hasErrors: false})
 
 async function postRegisterForm() {
+    console.log(username.value)
     response.value = await registerWithEmail(username.value, name.value, email.value, password.value);
     errors.value = response.value.errors;
 };
@@ -55,7 +56,53 @@ async function postRegisterForm() {
                                    :class="errors.has('name') ? ' border-red-500' : ''" placeholder="Name"/>
                         </div>
                     </div>
+                    <div class="rounded-md shadow-sm -space-y-px mb-1">
+                        <div>
+                            <label for="email-address" class="sr-only">Username</label>
+                            <input type="email" v-model="username" id="username" name="username" required
+                                   class="dark:bg-slate-500 dark:text-white dark:placeholder-white appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                   :class="errors.has('username') ? ' border-red-500' : ''" placeholder="username" />
+                        </div>
+                    </div>
+
+                    <div class="rounded-md shadow-sm -space-y-px mb-1">
+                        <div>
+                            <label for="email-address" class="sr-only">Email address</label>
+                            <input v-model="email" id="email-address" name="email" type="email" autocomplete="email" required
+                                   class="dark:bg-slate-500 dark:text-white dark:placeholder-white appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                   :class="errors.has('email') ? ' border-red-500' : ''" placeholder="Email address" />
+                        </div>
+                    </div>
+                    <div>
+                        <label for="password" class="sr-only">Password</label>
+                        <input v-model="password" id="password" name="password" type="password" autocomplete="current-password"
+                               required
+                               class="dark:bg-slate-500 dark:text-white dark:placeholder-white appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                               :class="errors.has('password') ? ' border-red-500' : ''" placeholder="Password" />
+                    </div>
+
+                    <div class="flex items-center justify-between">
+                        <div class="text-sm">
+                            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
+                                Forgot your password?
+                            </a>
+                        </div>
+                    </div>
                 </form>
+
+                <button @click.prevent="postRegisterForm"
+                        class="mt-5 group relative w-full flex justify-center py-2 px-4 border border-transparent
+                        text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                      <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+                        <!-- Heroicon name: solid/lock-closed -->
+                        <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg"
+                             viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                          <path fill-rule="evenodd"
+                                d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                                clip-rule="evenodd" />
+                        </svg>
+                      </span>Register
+                </button>
 
             </div>
         </div>
